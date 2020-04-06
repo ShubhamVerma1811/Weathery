fceaa46eb8c0e4828051724140b43aed7159e0cb
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-04-06 16:18:35 +0530

    Updates

---
+++ precache-manifest.d8ec547750b5ba989481f611c5174ed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfe5f59a5119c5d1dd191cea0a4c1a6",
+    "url": "/Weathery/index.html"
+  },
+  {
+    "revision": "e279b27e00a809cef920",
+    "url": "/Weathery/static/css/main.5b272846.chunk.css"
+  },
+  {
+    "revision": "ec99c40704e9b98ab75f",
+    "url": "/Weathery/static/js/2.e7a14a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weathery/static/js/2.e7a14a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e279b27e00a809cef920",
+    "url": "/Weathery/static/js/main.382dd99a.chunk.js"
+  },
+  {
+    "revision": "5836dc83e6e887f04061",
+    "url": "/Weathery/static/js/runtime-main.497b5145.js"
+  }
+]);
